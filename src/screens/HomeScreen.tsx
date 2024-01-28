9705e83bd3070b80dc23f2e493686560223a5da7
ramirezfernando <ramirez.fernando2003@gmail.com>
2024-01-27 21:46:44 -0600

    feat: add home screen and navigation

---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+
+function HomeScreen({ navigation }: any ) {
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity 
+        onPress={() => navigation.navigate('TODO: Open camera')}
+        style={[styles.option, { backgroundColor: '#F64610' }]}>
+        <Text style={styles.optionText}>split/IT</Text>
+        <Text style={styles.optionTextSmall}>split the bill amongst your party</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={[styles.option, { backgroundColor: '#FF8E6B' }]}>
+        <Text style={styles.optionText}>pay/IT</Text>
+        <Text style={styles.optionTextSmall}>pay your portion of the bill</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={[styles.option, { backgroundColor: '#FFFFFF' }]}>
+        <Text style={styles.optionTextWhiteBg}>ez/SPLIT</Text>        
+        <Text style={styles.optionTextSmallWhiteBg}>equally share the bill amongst your party</Text>
+
+      </TouchableOpacity>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+  },
+  option: {
+    width: '100%',
+    height: '33.5%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  optionText: {
+    fontSize: 24,
+    color: 'white',
+    fontWeight: 'bold',
+    paddingBottom: 10,
+  },
+  optionTextSmall: {
+    fontSize: 16,
+    color: 'white',
+  },
+  optionTextWhiteBg: {
+    fontSize: 24,
+    color: '#F05221',
+    fontWeight: 'bold',
+    paddingBottom: 10,
+  },
+  optionTextSmallWhiteBg: {
+    fontSize: 16,
+    color: '#F05221'
+  },
+});
+
+export default HomeScreen
